f15a947293445fc7acb9a9edd4d43c0e8995514c
Kamyil <plkamil1998@gmail.com>
2017-12-28 13:22:50 +0100

    Just importing

---
+++ parallax.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+	parallax();
+})
+
+function parallax(wScroll){
+wScroll = $(window).scrollTop();
+$('header').css('background-position','center '+(wScroll*0.45)+'px');
+$('.kontakt').css('background-position','center '+(wScroll*0.15)+'px');
+$('.pillars').css('background-position','center '+(wScroll*0.15)+'px');
+
+
+
+}
